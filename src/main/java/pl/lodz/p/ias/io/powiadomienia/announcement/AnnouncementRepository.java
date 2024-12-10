9aec039f5ea2b70b3bdf121ba50c5341f29135c9
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2024-12-10 15:42:03 +0100

    add: changes from main

--- src/main/java/pl/lodz/p/ias/io/powiadomienia/announcement/AnnouncementRepository.java
+++ src/main/java/pl/lodz/p/ias/io/powiadomienia/announcement/AnnouncementRepository.java
@@ -3,8 +3,6 @@ package pl.lodz.p.ias.io.powiadomienia.announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface AnnouncementRepository extends JpaRepository<Announcement, UUID> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 }
