037b020a97429bb5ffcad2ec5589944eb2197fa0
MrCin23 <marcin.targonski23@gmail.com>
2024-12-10 21:03:14 +0100

    Usunięcie zasobów z main

--- src/main/java/pl/lodz/p/ias/io/zasoby/repository/WarehouseRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.lodz.p.ias.io.zasoby.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.lodz.p.ias.io.zasoby.model.Warehouse;
-
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
-    //Warehouse findById(Long id);
-}
