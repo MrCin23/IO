66145913dcba92ab3a4e55d496cb8f68e247ddb8
obryja <bryja.ola@gmail.com>
2025-01-13 22:39:37 +0100

    quick fix

--- front/src/pages/uwierzytelnianie/AccountsListPage.tsx
+++ front/src/pages/uwierzytelnianie/AccountsListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from '../../api/Axios';
 import { Account } from '../../models/uwierzytelnianie/Account';
 
